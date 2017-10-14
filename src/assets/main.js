0c9df3fc83760bedf09233c07050e86941d726f9
mark_drastrup <mark.drastrup@gmail.com>
2017-10-14 16:03:12 +0200

    Adding message function

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,15 +3,22 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if(answer === "" && attempt === "") {
+        setHiddenFields();
+    }
 }
 
 //implement new functions here
 function setHiddenFields() {
     answer.value = Math.floor(Math.random() * 9999);
+    attemt.value = 0;
     var answerString = answer.value.toString();
     
     while(answerString.length < 4) {
           answer.value = "0" + answer.value;
     }
+}
+
+function setMessage(msg) {
+    document.getElementById("message").innerHTML = msg;
 }
